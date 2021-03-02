2a7de8536472ada864084f72f3353d30a5315a05
Lagashettik <Lagashettik@gmail.com>
2021-03-02 20:21:27 +0530

    Generified the function

--- src/main/java/FindMaximum.java
+++ src/main/java/FindMaximum.java
@@ -1,23 +1,8 @@
-public class FindMaximum {
-    int getIntegerMaximum(Integer number1, Integer number2, Integer number3){
-        if(number1.compareTo(number2) > 0 && number1.compareTo(number3) > 0)
-            return number1;
-        else if (number2.compareTo(number1) > 0 && number2.compareTo(number3) > 0)
-            return number2;
-        else
-            return number3;
-    }
+public class FindMaximum{
 
-    Float getFloatMaximum(Float number1, Float number2, Float number3){
-        if(number1.compareTo(number2) > 0 && number1.compareTo(number3) > 0)
-            return number1;
-        else if (number2.compareTo(number1) > 0 && number2.compareTo(number3) > 0)
-            return number2;
-        else
-            return number3;
-    }
 
-    String getStringMaximum(String number1, String number2, String number3){
+    @SuppressWarnings("rawtypes")
+    <T extends Comparable> T getMaximum(T number1, T number2, T number3){
         if(number1.compareTo(number2) > 0 && number1.compareTo(number3) > 0)
             return number1;
         else if (number2.compareTo(number1) > 0 && number2.compareTo(number3) > 0)
--- src/test/java/FindMaximumTest.java
+++ src/test/java/FindMaximumTest.java
@@ -6,57 +6,57 @@ public class FindMaximumTest {
     @Test
     public void IntegermaximumAtFirstPosition(){
         FindMaximum findMaximum = new FindMaximum();
-        int max = findMaximum.getIntegerMaximum(10,8,3);
+        int max = findMaximum.getMaximum(10,8,3);
         Assert.assertEquals(10,max);
     }
     @Test
     public void IntegermaximumAtSecondPosition(){
         FindMaximum findMaximum = new FindMaximum();
-        int max = findMaximum.getIntegerMaximum(8,10,3);
+        int max = findMaximum.getMaximum(8,10,3);
         Assert.assertEquals(10,max);
     }
     @Test
     public void IntegermaximumAtThirdPosition(){
         FindMaximum findMaximum = new FindMaximum();
-        int max = findMaximum.getIntegerMaximum(3,8,10);
+        int max = findMaximum.getMaximum(3,8,10);
         Assert.assertEquals(10,max);
     }
 
     @Test
     public void FloatmaximumAtFirstPosition(){
         FindMaximum findMaximum = new FindMaximum();
-        float max = findMaximum.getFloatMaximum(10.25f,8.5f,3.2f);
+        float max = findMaximum.getMaximum(10.25f,8.5f,3.2f);
         Assert.assertEquals(10.25,max,0.00);
     }
     @Test
     public void FloatmaximumAtSecondPosition(){
         FindMaximum findMaximum = new FindMaximum();
-        float max = findMaximum.getFloatMaximum(8.5f,10.25f,3.2f);
+        float max = findMaximum.getMaximum(8.5f,10.25f,3.2f);
         Assert.assertEquals(10.25,max,0.00);
     }
     @Test
     public void FloatmaximumAtThirdPosition(){
         FindMaximum findMaximum = new FindMaximum();
-        float max = findMaximum.getFloatMaximum(3.2f,8.5f,10.25f);
+        float max = findMaximum.getMaximum(3.2f,8.5f,10.25f);
         Assert.assertEquals(10.25,max,0.00);
     }
 
     @Test
     public void StringmaximumAtFirstPosition(){
         FindMaximum findMaximum = new FindMaximum();
-        String max = findMaximum.getStringMaximum("Peach","Apple","Banana");
+        String max = findMaximum.getMaximum("Peach","Apple","Banana");
         Assert.assertEquals("Peach",max);
     }
     @Test
     public void StringmaximumAtSecondPosition(){
         FindMaximum findMaximum = new FindMaximum();
-        String max = findMaximum.getStringMaximum("Apple","Peach","Banana");
+        String max = findMaximum.getMaximum("Apple","Peach","Banana");
         Assert.assertEquals("Peach",max);
     }
     @Test
     public void StringmaximumAtThirdPosition(){
         FindMaximum findMaximum = new FindMaximum();
-        String max = findMaximum.getStringMaximum("Apple","Banana","Peach");
+        String max = findMaximum.getMaximum("Apple","Banana","Peach");
         Assert.assertEquals("Peach",max);
     }
 }
