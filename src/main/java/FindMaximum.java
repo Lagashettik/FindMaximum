dd4284e12b5d2abe1e0e6190b80bcf2f3f211d56
Lagashettik <Lagashettik@gmail.com>
2021-03-02 18:33:56 +0530

    Written function to find maximum float number

--- src/main/java/FindMaximum.java
+++ src/main/java/FindMaximum.java
@@ -1,5 +1,5 @@
 public class FindMaximum {
-    int getMaximum(Integer number1, Integer number2, Integer number3){
+    int getIntegerMaximum(Integer number1, Integer number2, Integer number3){
         if(number1.compareTo(number2) > 0 && number1.compareTo(number3) > 0)
             return number1;
         else if (number2.compareTo(number1) > 0 && number2.compareTo(number3) > 0)
@@ -7,4 +7,14 @@ public class FindMaximum {
         else
             return number3;
     }
+
+    Float getFloatMaximum(Float number1, Float number2, Float number3){
+        if(number1.compareTo(number2) > 0 && number1.compareTo(number3) > 0)
+            return number1;
+        else if (number2.compareTo(number1) > 0 && number2.compareTo(number3) > 0)
+            return number2;
+        else
+            return number3;
+    }
+
 }
--- src/test/java/FindMaximumTest.java
+++ src/test/java/FindMaximumTest.java
@@ -4,21 +4,40 @@ import org.junit.Test;
 public class FindMaximumTest {
 
     @Test
-    public void maximumAtFirstPosition(){
+    public void IntegermaximumAtFirstPosition(){
         FindMaximum findMaximum = new FindMaximum();
-        int max = findMaximum.getMaximum(10,8,3);
+        int max = findMaximum.getIntegerMaximum(10,8,3);
         Assert.assertEquals(10,max);
     }
     @Test
-    public void maximumAtSecondPosition(){
+    public void IntegermaximumAtSecondPosition(){
         FindMaximum findMaximum = new FindMaximum();
-        int max = findMaximum.getMaximum(8,10,3);
+        int max = findMaximum.getIntegerMaximum(8,10,3);
         Assert.assertEquals(10,max);
     }
     @Test
-    public void maximumAtThirdPosition(){
+    public void IntegermaximumAtThirdPosition(){
         FindMaximum findMaximum = new FindMaximum();
-        int max = findMaximum.getMaximum(3,8,10);
+        int max = findMaximum.getIntegerMaximum(3,8,10);
         Assert.assertEquals(10,max);
+    }
+
+    @Test
+    public void FloatmaximumAtFirstPosition(){
+        FindMaximum findMaximum = new FindMaximum();
+        float max = findMaximum.getFloatMaximum(10.25f,8.5f,3.2f);
+        Assert.assertEquals(10.25,max,0.00);
+    }
+    @Test
+    public void FloatmaximumAtSecondPosition(){
+        FindMaximum findMaximum = new FindMaximum();
+        float max = findMaximum.getFloatMaximum(8.5f,10.25f,3.2f);
+        Assert.assertEquals(10.25,max,0.00);
+    }
+    @Test
+    public void FloatmaximumAtThirdPosition(){
+        FindMaximum findMaximum = new FindMaximum();
+        float max = findMaximum.getFloatMaximum(3.2f,8.5f,10.25f);
+        Assert.assertEquals(10.25,max,0.00);
     }
 }
