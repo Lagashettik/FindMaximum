ca4e31536fc3e2d6f8d63e6beeb14b06f03e9321
Lagashettik <Lagashettik@gmail.com>
2021-03-02 18:17:43 +0530

    Finded maximum from three numbers

--- src/main/java/FindMaximum.java
+++ src/main/java/FindMaximum.java
@@ -1,3 +1,10 @@
 public class FindMaximum {
-
+    int getMaximum(Integer number1, Integer number2, Integer number3){
+        if(number1.compareTo(number2) > 0 && number1.compareTo(number3) > 0)
+            return number1;
+        else if (number2.compareTo(number1) > 0 && number2.compareTo(number3) > 0)
+            return number2;
+        else
+            return number3;
+    }
 }
--- src/test/java/FindMaximumTest.java
+++ src/test/java/FindMaximumTest.java
@@ -1,3 +1,24 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class FindMaximumTest {
 
+    @Test
+    public void maximumAtFirstPosition(){
+        FindMaximum findMaximum = new FindMaximum();
+        int max = findMaximum.getMaximum(10,8,3);
+        Assert.assertEquals(10,max);
+    }
+    @Test
+    public void maximumAtSecondPosition(){
+        FindMaximum findMaximum = new FindMaximum();
+        int max = findMaximum.getMaximum(8,10,3);
+        Assert.assertEquals(10,max);
+    }
+    @Test
+    public void maximumAtThirdPosition(){
+        FindMaximum findMaximum = new FindMaximum();
+        int max = findMaximum.getMaximum(3,8,10);
+        Assert.assertEquals(10,max);
+    }
 }
